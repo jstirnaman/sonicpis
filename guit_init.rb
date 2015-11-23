d52fa2eea4ccb82498babdfc3f3c3ec25098d299
Jason Stirnaman <stirnamanj@gmail.com>
2015-11-23 00:38:16 +0000

    Make :guit sync with :boom

---
+++ guit_init.rb
@@ -0,0 +1,71 @@
+def guit_a(opts)
+  sample :guit_em9, rate: opts[:rate]
+end
+
+def p_c
+  2.times do
+    play :C3, pan: -1, amp: 1
+    sleep 0.5
+  end
+  2.times do
+    play :D2, pan: 1, amp: 1, release: 3
+    sleep 0.5
+  end
+  sleep 4
+end
+
+in_thread do
+  live_loop :guit do
+    4.times do
+      with_fx :echo, mix: 0.6, phase: 0.25 do
+        sample :guit_em9, rate: -0.5
+      end
+      sleep 8
+    end
+
+    loop do
+      with_fx :echo, mix: 0.3, phase: 0.25 do
+        sample :guit_em9, rate: -0.5
+      end
+      sleep 8
+    end
+  end
+end
+
+live_loop :boom do
+  sync :guit
+  with_fx :reverb, room: 1 do
+    sample :bd_boom, amp: 10, rate: 1
+  end
+  sleep 8
+end
+
+in_thread do
+  sync :guit
+  16.times do
+    4.times do
+      guit_a({rate: 0.5})
+      sleep 8
+    end
+
+    loop do
+      sleep 4
+      p_c
+      guit_a({rate: 1})
+
+      sleep 4
+      8.times do
+        8.times do
+          play chord(:E3, :minor).choose, pan: -1, release: 2, attack: 2 
+          sleep 0.2
+        end
+
+        8.times do
+          play chord(:E3, :minor).choose, pan: 1, release: 2, attack: 2
+          sleep 0.2
+        end
+        sleep 4
+      end
+    end
+  end
+end
